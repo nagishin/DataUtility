d321cb0fc4c0ebfa23fd12c3e92406e1e812e0a9
Nagi <37642101+nagishin@users.noreply.github.com>
2021-03-08 16:43:33 +0900

    update.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup
 setup(
     name='DataUtility',
     packages=['DataUtility'],
-    version='2.0.0',
+    version='1.0.0',
     author='Nagi',
     url='https://github.com/nagishin/DataUtility.git',
-    install_requires=['requests', 'datetime', 'pytz', 'numpy', 'pandas', 'pybybit==2.0.0'],
     dependency_links=['git+https://github.com/MtkN1/pybybit.git#egg=pybybit-2.0.0'],
+    install_requires=['requests', 'datetime', 'pytz', 'numpy', 'pandas', 'pybybit==2.0.0'],
 )
